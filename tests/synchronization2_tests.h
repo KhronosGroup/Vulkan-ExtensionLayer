2668c98236293bce2dcadc9c2951f0ac6dd6d018
Jeremy Gebben <jeremyg@lunarg.com>
2021-02-25 10:45:45 -0700

    tests: Add Sync2CompatTest.Vulkan10
    This test enables synchronization2 with vulkan 1.0 and no other
    extensions. It currently generates the following validation
    errors:
    
    VUID-vkCreateDevice-ppEnabledExtensionNames-01387
    Missing extensions required by the device extension
    VK_KHR_synchronization2: VK_KHR_get_physical_device_properties2,
    VK_KHR_create_renderpass2.
    
    UNASSIGNED-GeneralParameterError-ExtensionNotEnabled
    Attemped to call vkQueueSubmit2KHR() but its required extension
    VK_KHR_create_renderpass2 has not been enabled
    
    UNASSIGNED-GeneralParameterError-ExtensionNotEnabled
    Attemped to call vkQueueSubmit2KHR() but its required extension
    VK_KHR_get_physical_device_properties2 has not been enabled
    
    But shows how the extension layer can work on a 1.0 device without
    support for the above extensions.
    
    Change-Id: If152beadf5659bdcb0618bc682f32349682f86f8

--- tests/synchronization2_tests.h
+++ tests/synchronization2_tests.h
@@ -13,3 +13,5 @@ class Sync2Test : public VkExtensionLayerTest {
     void ValidOwnershipTransfer(ErrorMonitor *monitor, VkCommandBufferObj *cb_from, VkCommandBufferObj *cb_to,
                                 const VkBufferMemoryBarrier2KHR *buf_barrier, const VkImageMemoryBarrier2KHR *img_barrier);
 };
+
+class Sync2CompatTest : public VkExtensionLayerTest {};
