ffa141e2976b736acbdf1c33e887db03f05a5b9e
Jeremy Gebben <jeremyg@lunarg.com>
2021-10-06 08:40:56 -0600

    build: Update Vulkan-Headers to 1.2.195
    Also update codegen scripts to match latest in Vulkan-ValidationLayers.

--- scripts/helper_file_generator.py
+++ scripts/helper_file_generator.py
@@ -55,7 +55,8 @@ class HelperFileOutputGeneratorOptions(GeneratorOptions):
                  alignFuncParam = 0,
                  library_name = '',
                  expandEnumerants = True,
-                 helper_file_type = ''):
+                 helper_file_type = '',
+                 valid_usage_path = ''):
         GeneratorOptions.__init__(self,
                 conventions = conventions,
                 filename = filename,
@@ -81,6 +82,7 @@ class HelperFileOutputGeneratorOptions(GeneratorOptions):
         self.alignFuncParam   = alignFuncParam
         self.library_name     = library_name
         self.helper_file_type = helper_file_type
+        self.valid_usage_path = valid_usage_path
 #
 # HelperFileOutputGenerator - subclass of OutputGenerator. Outputs Vulkan helper files
 class HelperFileOutputGenerator(OutputGenerator):
@@ -222,10 +224,6 @@ void CoreChecksOptickInstrumented::PreCallRecordQueuePresentKHR(VkQueue queue, c
         if interface.tag != 'extension':
             return
         name = self.featureName
-        nameElem = interface[0][1]
-        name_define = nameElem.get('name')
-        if 'EXTENSION_NAME' not in name_define:
-            print("Error in vk.xml file -- extension name is not available")
         requires = interface.get('requires')
         if requires is not None:
             required_extensions = requires.split(',')
@@ -234,7 +232,7 @@ void CoreChecksOptickInstrumented::PreCallRecordQueuePresentKHR(VkQueue queue, c
         requiresCore = interface.get('requiresCore')
         if requiresCore is not None:
             required_extensions.append('VK_VERSION_%s' % ('_'.join(requiresCore.split('.'))))
-        info = { 'define': name_define, 'ifdef':self.featureExtraProtect, 'reqs':required_extensions }
+        info = { 'define': GetNameDefine(interface), 'ifdef':self.featureExtraProtect, 'reqs':required_extensions }
         if interface.get('type') == 'instance':
             self.instance_extension_info[name] = info
         else:
@@ -737,62 +735,62 @@ void CoreChecksOptickInstrumented::PreCallRecordQueuePresentKHR(VkQueue queue, c
     def GenerateExtensionHelperHeader(self):
 
         V_1_0_instance_extensions_promoted_to_V_1_1_core = [
-            'vk_khr_device_group_creation',
-            'vk_khr_external_fence_capabilities',
-            'vk_khr_external_memory_capabilities',
-            'vk_khr_external_semaphore_capabilities',
-            'vk_khr_get_physical_device_properties_2',
+            'VK_KHR_device_group_creation',
+            'VK_KHR_external_fence_capabilities',
+            'VK_KHR_external_memory_capabilities',
+            'VK_KHR_external_semaphore_capabilities',
+            'VK_KHR_get_physical_device_properties2',
             ]
 
         V_1_0_device_extensions_promoted_to_V_1_1_core = [
-            'vk_khr_16bit_storage',
-            'vk_khr_bind_memory_2',
-            'vk_khr_dedicated_allocation',
-            'vk_khr_descriptor_update_template',
-            'vk_khr_device_group',
-            'vk_khr_external_fence',
-            'vk_khr_external_memory',
-            'vk_khr_external_semaphore',
-            'vk_khr_get_memory_requirements_2',
-            'vk_khr_maintenance1',
-            'vk_khr_maintenance2',
-            'vk_khr_maintenance3',
-            'vk_khr_multiview',
-            'vk_khr_relaxed_block_layout',
-            'vk_khr_sampler_ycbcr_conversion',
-            'vk_khr_shader_draw_parameters',
-            'vk_khr_storage_buffer_storage_class',
-            'vk_khr_variable_pointers',
+             'VK_KHR_16bit_storage',
+             'VK_KHR_bind_memory2',
+             'VK_KHR_dedicated_allocation',
+             'VK_KHR_descriptor_update_template',
+             'VK_KHR_device_group',
+             'VK_KHR_external_fence',
+             'VK_KHR_external_memory',
+             'VK_KHR_external_semaphore',
+             'VK_KHR_get_memory_requirements2',
+             'VK_KHR_maintenance1',
+             'VK_KHR_maintenance2',
+             'VK_KHR_maintenance3',
+             'VK_KHR_multiview',
+             'VK_KHR_relaxed_block_layout',
+             'VK_KHR_sampler_ycbcr_conversion',
+             'VK_KHR_shader_draw_parameters',
+             'VK_KHR_storage_buffer_storage_class',
+             'VK_KHR_variable_pointers',
             ]
 
         V_1_1_instance_extensions_promoted_to_V_1_2_core = [
             ]
 
         V_1_1_device_extensions_promoted_to_V_1_2_core = [
-            'vk_khr_8bit_storage',
-            'vk_khr_buffer_device_address',
-            'vk_khr_create_renderpass_2',
-            'vk_khr_depth_stencil_resolve',
-            'vk_khr_draw_indirect_count',
-            'vk_khr_driver_properties',
-            'vk_khr_image_format_list',
-            'vk_khr_imageless_framebuffer',
-            'vk_khr_sampler_mirror_clamp_to_edge',
-            'vk_khr_separate_depth_stencil_layouts',
-            'vk_khr_shader_atomic_int64',
-            'vk_khr_shader_float16_int8',
-            'vk_khr_shader_float_controls',
-            'vk_khr_shader_subgroup_extended_types',
-            'vk_khr_spirv_1_4',
-            'vk_khr_timeline_semaphore',
-            'vk_khr_uniform_buffer_standard_layout',
-            'vk_khr_vulkan_memory_model',
-            'vk_ext_descriptor_indexing',
-            'vk_ext_host_query_reset',
-            'vk_ext_sampler_filter_minmax',
-            'vk_ext_scalar_block_layout',
-            'vk_ext_separate_stencil_usage',
-            'vk_ext_shader_viewport_index_layer',
+            'VK_KHR_8bit_storage',
+            'VK_KHR_buffer_device_address',
+            'VK_KHR_create_renderpass2',
+            'VK_KHR_depth_stencil_resolve',
+            'VK_KHR_draw_indirect_count',
+            'VK_KHR_driver_properties',
+            'VK_KHR_image_format_list',
+            'VK_KHR_imageless_framebuffer',
+            'VK_KHR_sampler_mirror_clamp_to_edge',
+            'VK_KHR_separate_depth_stencil_layouts',
+            'VK_KHR_shader_atomic_int64',
+            'VK_KHR_shader_float16_int8',
+            'VK_KHR_shader_float_controls',
+            'VK_KHR_shader_subgroup_extended_types',
+            'VK_KHR_spirv_1_4',
+            'VK_KHR_timeline_semaphore',
+            'VK_KHR_uniform_buffer_standard_layout',
+            'VK_KHR_vulkan_memory_model',
+            'VK_EXT_descriptor_indexing',
+            'VK_EXT_host_query_reset',
+            'VK_EXT_sampler_filter_minmax',
+            'VK_EXT_scalar_block_layout',
+            'VK_EXT_separate_stencil_usage',
+            'VK_EXT_shader_viewport_index_layer',
             ]
 
         output = [
@@ -824,18 +822,15 @@ void CoreChecksOptickInstrumented::PreCallRecordQueuePresentKHR(VkQueue queue, c
             '    kEnabledByApiLevel,',
             '};',
             '',
-            'static bool DECORATE_UNUSED IsExtEnabled(ExtEnabled feature) {',
-            '    if (feature == kNotEnabled) return false;',
-            '    return true;',
+            'static bool DECORATE_UNUSED IsExtEnabled(ExtEnabled extension) {',
+            '    return (extension != kNotEnabled);',
+            '};',
+            '',
+            'static bool DECORATE_UNUSED IsExtEnabledByCreateinfo(ExtEnabled extension) {',
+            '    return (extension == kEnabledByCreateinfo);',
             '};',
             '#define VK_VERSION_1_2_NAME "VK_VERSION_1_2"',
             '']
-
-        def guarded(ifdef, value):
-            if ifdef is not None:
-                return '\n'.join([ '#ifdef %s' % ifdef, value, '#endif' ])
-            else:
-                return value
 
         for type in ['Instance', 'Device']:
             struct_type = '%sExtensions' % type
@@ -853,7 +848,7 @@ void CoreChecksOptickInstrumented::PreCallRecordQueuePresentKHR(VkQueue queue, c
 
             extension_items = sorted(extension_dict.items())
 
-            field_name = { ext_name: re.sub('_extension_name', '', info['define'].lower()) for ext_name, info in extension_items }
+            field_name = { ext_name: ext_name.lower() for ext_name, info in extension_items }
 
             # Add in pseudo-extensions for core API versions so real extensions can depend on them
             extension_dict['VK_VERSION_1_2'] = {'define':"VK_VERSION_1_2_NAME", 'ifdef':None, 'reqs':[]}
@@ -912,7 +907,7 @@ void CoreChecksOptickInstrumented::PreCallRecordQueuePresentKHR(VkQueue queue, c
                 reqs = req_join.join([req_format % (field_name[req], extension_dict[req]['define']) for req in info['reqs']])
                 return info_format % (info['define'], field_name[ext_name], '{%s}' % (req_indent + reqs) if reqs else '')
 
-            struct.extend([guarded(info['ifdef'], format_info(ext_name, info)) for ext_name, info in extension_items])
+            struct.extend([Guarded(info['ifdef'], format_info(ext_name, info)) for ext_name, info in extension_items])
             struct.extend([
                 '        };',
                 '',
@@ -956,11 +951,11 @@ void CoreChecksOptickInstrumented::PreCallRecordQueuePresentKHR(VkQueue queue, c
             struct.extend([
                 '',
                 '        static const std::vector<const char *> V_1_1_promoted_%s_apis = {' % type.lower() ])
-            struct.extend(['            %s_EXTENSION_NAME,' % ext_name.upper() for ext_name in promoted_1_1_ext_list])
+            struct.extend(['            %s,' % extension_dict[ext_name]['define'] for ext_name in promoted_1_1_ext_list])
             struct.extend([
                 '        };',
                 '        static const std::vector<const char *> V_1_2_promoted_%s_apis = {' % type.lower() ])
-            struct.extend(['            %s_EXTENSION_NAME,' % ext_name.upper() for ext_name in promoted_1_2_ext_list])
+            struct.extend(['            %s,' % extension_dict[ext_name]['define'] for ext_name in promoted_1_2_ext_list])
             struct.extend([
                 '        };',
                 '',
@@ -998,7 +993,7 @@ void CoreChecksOptickInstrumented::PreCallRecordQueuePresentKHR(VkQueue queue, c
 
             # Output reference lists of instance/device extension names
             struct.extend(['', 'static const std::set<std::string> k%sExtensionNames = {' % type])
-            struct.extend([guarded(info['ifdef'], '    %s,' % info['define']) for ext_name, info in extension_items])
+            struct.extend([Guarded(info['ifdef'], '    %s,' % info['define']) for ext_name, info in extension_items])
             struct.extend(['};', ''])
             output.extend(struct)
 
@@ -1471,7 +1466,13 @@ void CoreChecksOptickInstrumented::PreCallRecordQueuePresentKHR(VkQueue queue, c
                     '        pTessellationState = new safe_VkPipelineTessellationStateCreateInfo(in_struct->pTessellationState);\n'
                     '    else\n'
                     '        pTessellationState = NULL; // original pTessellationState pointer ignored\n'
-                    '    bool has_rasterization = in_struct->pRasterizationState ? !in_struct->pRasterizationState->rasterizerDiscardEnable : false;\n'
+                    '    bool is_dynamic_has_rasterization = false;\n'
+                    '    if (in_struct->pDynamicState && in_struct->pDynamicState->pDynamicStates) {\n'
+                    '        for (uint32_t i = 0; i < in_struct->pDynamicState->dynamicStateCount && !is_dynamic_has_rasterization; ++i)\n'
+                    '            if (in_struct->pDynamicState->pDynamicStates[i] == VK_DYNAMIC_STATE_RASTERIZER_DISCARD_ENABLE_EXT)\n'
+                    '                is_dynamic_has_rasterization = true;\n'
+                    '    }\n'
+                    '    bool has_rasterization = in_struct->pRasterizationState ? (is_dynamic_has_rasterization || !in_struct->pRasterizationState->rasterizerDiscardEnable) : false;\n'
                     '    if (in_struct->pViewportState && has_rasterization) {\n'
                     '        bool is_dynamic_viewports = false;\n'
                     '        bool is_dynamic_scissors = false;\n'
@@ -1522,6 +1523,14 @@ void CoreChecksOptickInstrumented::PreCallRecordQueuePresentKHR(VkQueue queue, c
                     '    }\n'
                     '    else\n'
                     '        pScissors = NULL;\n',
+                # VkFrameBufferCreateInfo is special case because its pAttachments pointer may be non-null but ignored
+                'VkFramebufferCreateInfo' :
+                    '    if (attachmentCount && in_struct->pAttachments && !(flags & VK_FRAMEBUFFER_CREATE_IMAGELESS_BIT)) {\n'
+                    '        pAttachments = new VkImageView[attachmentCount];\n'
+                    '        for (uint32_t i = 0; i < attachmentCount; ++i) {\n'
+                    '            pAttachments[i] = in_struct->pAttachments[i];\n'
+                    '        }\n'
+                    '    }\n',
                 # VkDescriptorSetLayoutBinding is special case because its pImmutableSamplers pointer may be non-null but ignored
                 'VkDescriptorSetLayoutBinding' :
                     '    const bool sampler_type = in_struct->descriptorType == VK_DESCRIPTOR_TYPE_SAMPLER || in_struct->descriptorType == VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;\n'
@@ -1574,7 +1583,13 @@ void CoreChecksOptickInstrumented::PreCallRecordQueuePresentKHR(VkQueue queue, c
                     '        pTessellationState = new safe_VkPipelineTessellationStateCreateInfo(*copy_src.pTessellationState);\n'
                     '    else\n'
                     '        pTessellationState = NULL; // original pTessellationState pointer ignored\n'
-                    '    bool has_rasterization = copy_src.pRasterizationState ? !copy_src.pRasterizationState->rasterizerDiscardEnable : false;\n'
+                    '    bool is_dynamic_has_rasterization = false;\n'
+                    '    if (copy_src.pDynamicState && copy_src.pDynamicState->pDynamicStates) {\n'
+                    '        for (uint32_t i = 0; i < copy_src.pDynamicState->dynamicStateCount && !is_dynamic_has_rasterization; ++i)\n'
+                    '            if (copy_src.pDynamicState->pDynamicStates[i] == VK_DYNAMIC_STATE_RASTERIZER_DISCARD_ENABLE_EXT)\n'
+                    '                is_dynamic_has_rasterization = true;\n'
+                    '    }\n'
+                    '    bool has_rasterization = copy_src.pRasterizationState ? (is_dynamic_has_rasterization || !copy_src.pRasterizationState->rasterizerDiscardEnable) : false;\n'
                     '    if (copy_src.pViewportState && has_rasterization) {\n'
                     '        pViewportState = new safe_VkPipelineViewportStateCreateInfo(*copy_src.pViewportState);\n'
                     '    } else\n'
@@ -1614,6 +1629,14 @@ void CoreChecksOptickInstrumented::PreCallRecordQueuePresentKHR(VkQueue queue, c
                     '    }\n'
                     '    else\n'
                     '        pScissors = NULL;\n',
+                'VkFramebufferCreateInfo' :
+                    '    pNext = SafePnextCopy(copy_src.pNext);\n'
+                    '    if (attachmentCount && copy_src.pAttachments && !(flags & VK_FRAMEBUFFER_CREATE_IMAGELESS_BIT)) {\n'
+                    '        pAttachments = new VkImageView[attachmentCount];\n'
+                    '        for (uint32_t i = 0; i < attachmentCount; ++i) {\n'
+                    '            pAttachments[i] = copy_src.pAttachments[i];\n'
+                    '        }\n'
+                    '    }\n',
                 'VkAccelerationStructureBuildGeometryInfoKHR':
                     '    if (geometryCount) {\n'
                     '        if ( copy_src.ppGeometries) {\n'
@@ -1640,7 +1663,7 @@ void CoreChecksOptickInstrumented::PreCallRecordQueuePresentKHR(VkQueue queue, c
                     '             delete ppGeometries[i];\n'
                     '        }\n'
                     '        delete[] ppGeometries;\n'
-                    '    } else {\n'
+                    '    } else if(pGeometries) {\n'
                     '        delete[] pGeometries;\n'
                     '    }\n'
            }
