e97bcc5f606bf3901a6cfac827f50aa1501519da
Jeremy Gebben <jeremyg@lunarg.com>
2023-09-25 10:30:09 -0600

    build: Update Vulkan-Headers and VUL to 1.3.265
    No codegen changes from this update, but VUL functions got
    renamed from vl* to vku*.

--- layers/decompression/decompression.cpp
+++ layers/decompression/decompression.cpp
@@ -248,41 +248,41 @@ static VkLayerInstanceCreateInfo* GetChainInfo(const VkInstanceCreateInfo* pCrea
 void InitLayerSettings(const VkInstanceCreateInfo* pCreateInfo, const VkAllocationCallbacks* pAllocator, LayerSettings* layer_settings) {
     assert(layer_settings != nullptr);
 
-    const VkLayerSettingsCreateInfoEXT* create_info = vlFindLayerSettingsCreateInfo(pCreateInfo);
+    const VkLayerSettingsCreateInfoEXT* create_info = vkuFindLayerSettingsCreateInfo(pCreateInfo);
 
-    VlLayerSettingSet layer_setting_set = VK_NULL_HANDLE;
-    vlCreateLayerSettingSet(kGlobalLayer.layerName, create_info, pAllocator, nullptr, &layer_setting_set);
+    VkuLayerSettingSet layer_setting_set = VK_NULL_HANDLE;
+    vkuCreateLayerSettingSet(kGlobalLayer.layerName, create_info, pAllocator, nullptr, &layer_setting_set);
 
     static const char* setting_names[] = {kLayerSettingsForceEnable, kLayerSettingsLogging, kLayerSettingsCustomSTypeInfo};
     uint32_t setting_name_count = static_cast<uint32_t>(std::size(setting_names));
 
     uint32_t unknown_setting_count = 0;
-    vlGetUnknownSettings(create_info, setting_name_count, setting_names, &unknown_setting_count, nullptr);
+    vkuGetUnknownSettings(create_info, setting_name_count, setting_names, &unknown_setting_count, nullptr);
 
     if (unknown_setting_count > 0) {
         std::vector<const char*> unknown_settings;
         unknown_settings.resize(unknown_setting_count);
 
-        vlGetUnknownSettings(create_info, setting_name_count, setting_names, &unknown_setting_count, &unknown_settings[0]);
+        vkuGetUnknownSettings(create_info, setting_name_count, setting_names, &unknown_setting_count, &unknown_settings[0]);
 
         for (std::size_t i = 0, n = unknown_settings.size(); i < n; ++i) {
             LOG("Unknown %s setting listed in VkLayerSettingsCreateInfoEXT, this setting is ignored.\n", unknown_settings[i]);
         }
     }
 
-    if (vlHasLayerSetting(layer_setting_set, kLayerSettingsForceEnable)) {
-        vlGetLayerSettingValue(layer_setting_set, kLayerSettingsForceEnable, layer_settings->force_enable);
+    if (vkuHasLayerSetting(layer_setting_set, kLayerSettingsForceEnable)) {
+        vkuGetLayerSettingValue(layer_setting_set, kLayerSettingsForceEnable, layer_settings->force_enable);
     }
 
-    if (vlHasLayerSetting(layer_setting_set, kLayerSettingsLogging)) {
-        vlGetLayerSettingValue(layer_setting_set, kLayerSettingsLogging, layer_settings->logging);
+    if (vkuHasLayerSetting(layer_setting_set, kLayerSettingsLogging)) {
+        vkuGetLayerSettingValue(layer_setting_set, kLayerSettingsLogging, layer_settings->logging);
     }
 
-    if (vlHasLayerSetting(layer_setting_set, kLayerSettingsCustomSTypeInfo)) {
-        vlGetLayerSettingValues(layer_setting_set, kLayerSettingsCustomSTypeInfo, custom_stype_info);
+    if (vkuHasLayerSetting(layer_setting_set, kLayerSettingsCustomSTypeInfo)) {
+        vkuGetLayerSettingValues(layer_setting_set, kLayerSettingsCustomSTypeInfo, custom_stype_info);
     }
 
-    vlDestroyLayerSettingSet(layer_setting_set, pAllocator);
+    vkuDestroyLayerSettingSet(layer_setting_set, pAllocator);
 }
 
 VKAPI_ATTR VkResult VKAPI_CALL CreateInstance(const VkInstanceCreateInfo* pCreateInfo, const VkAllocationCallbacks* pAllocator,
